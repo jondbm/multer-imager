672e8690d6038fb78fafa9efc534c960fbb2b85c
Jonathan Miller <jon.miller@novafabrik.com>
2016-06-30 19:48:55 +0100

    f

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ function S3Storage(opts) {
     throw new Error('gm is required');
   }
   this.options = opts;
-  console.log('OGM=')
-  console.log(opts.gm)
   this.options.filename = (opts.filename || getFilename);
   this.s3fs = new S3FS(opts.bucket, opts);
-  console.log('CONVERT GOES HERE!')
+  //this.s3fs2 = new S3FS(opts.bucket, opts);
   this.convert = gm(opts.gm);
-  //this.convert = gm({scale:300})
+  // this.convertx = gmx(opts.gm);
+  //this.convert2 = gm(opts.gm2);
 }
 
 function getFilename(req, file, cb) {
@@ -38,24 +37,51 @@ function getFilename(req, file, cb) {
 }
 
 S3Storage.prototype._handleFile = function(req, file, cb) {
+  console.log('FFFILE=')
+  console.log(file)
   var self = this;
+  //var file2 = file
   self.options.filename(req, file, function(err, filename) {
     if (err) {
+      console.log('CB ERROR!')
       return cb(err);
     }
-    var filePath = self.options.dirname + '/' + filename;
+    var filePath = self.options.dirname + '' + filename;
+    console.log('OPTIONSELF=')
+    console.log(self.options.dirname)
+    console.log('FILEPATH=='+filePath)
+    //var filePath2 = self.options.dirname + '/thumb_' + filename;
     var outStream = self.s3fs.createWriteStream(filePath);
+    console.log('OT==')
+    console.log(outStream)
+    console.log('FSTRSMA:')
+    console.log(file.stream)
+    //var outStream2 = self.s3fs2.createWriteStream(filePath2);
     file.stream
-      .pipe(self.convert())
+      //.pipe(self.convert())
       .pipe(outStream);
+    /*file.stream
+      .pipe(self.convert2())
+      .pipe(outStream2);*/
     outStream.on('error', cb);
-    outStream.on('finish', function() {
-      cb(null, {
-        size: outStream.bytesWritten,
-        key: filePath,
-        location: 'https://' + self.options.bucket + '.s3.amazonaws.com/' + filePath
+    //outStream.on('finish', function() {
+      outStream.on('finish', function() {
+        cb(null, {
+          size: outStream.bytesWritten,
+          key: filePath,
+          location: 'https://' + self.options.bucket + '.s3.amazonaws.com/' + filePath
+        });
       });
-    });
+      /*outStream2.on('finish', function() {
+        /*cb(null, {
+          size: outStream2.bytesWritten,
+          key: filePath2,
+          location: 'https://' + self.options.bucket + '.s3.amazonaws.com/' + filePath2
+        });*/
+      //});
+    //});
+    //outStream2.on('error', cb);
+    
   });
 };
 
